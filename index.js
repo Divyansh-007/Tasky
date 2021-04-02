14744df82abf3ec5e20d6434136c2fdb338815e4
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-04-02 17:56:45 +0530

    Set up view engine and layouts

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const express = require('express');
 
 const port = 8000;
 
+// to use layouts
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
+
+// use static files
+app.use(express.static('./assets'));
+
+// to extract styles and scripts of subpages in layouts
+app.set('layouts extractStyles',true);
+app.set('layouts extractScripts',true);
+
+app.use(expressLayouts);
+
+// set up view engine and views
+app.use('view engine','ejs');
+app.use('views', './views'); 
 
 app.listen(port,function(err){
     if(err){console.log(`Error in running the server: ${err}`);}
