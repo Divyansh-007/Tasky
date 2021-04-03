31891b7451276689ab309ad5c18ff54ec3503d37
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-04-03 18:17:15 +0530

    Set up Passport Local Strategy

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,64 @@
+// required libraries and models
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+
+// new authentication
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'email'
+    },
+    function(email,password,done){
+        User.findOne({email : email},function(err,user){
+            if(err){
+                console.log('error in finding the user');
+                return done(err);
+            }
+
+            if(!user || user.password != password){
+                console.log('Invalid Email/Password');
+                return done(null,false);
+            }
+
+            return done(null,user);
+        });
+    }
+));
+
+// serializing the user id for cookie
+passport.serializeUser(function(user,done){
+    return done(null,user.id);
+});
+
+// deserializing the user id from cookie
+passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+        if(err){
+            console.log('error in finding the user');
+            return done(err);
+        }
+
+        return done(null,user);
+    });
+});
+
+// checking authentication
+passport.checkAuthenticatedUser = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+
+    return res.redirect('/user/sign-in');
+}
+
+// setting authenticated user
+passport.setAuthenticatedUser = function(req, res, next){
+    if(req.isAuthenticated()){
+        res.locals.user = req.user;
+    }
+
+    next();
+}
+
+module.exports = passport;
