5d91cc84fa902bacf116c9dcab50356d7d41f9bb
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-04-22 19:11:56 +0530

    Make required changes to controller and changes memo

--- controllers/task_controller.js
+++ controllers/task_controller.js
@@ -1,23 +1,47 @@
-// importing model to be used
+// importing model and libraries to be used
 const Task = require('../models/task');
+const taskMailer = require('../mailers/task_mailer');
 
-// create task
-module.exports.create = function(req,res){
-    Task.create({
-        description: req.body.description,
-        category: req.body.category,
-        date: req.body.date,
-        status: 'incomplete',
-        user: req.user._id
-    },function(err){
-        if(err){
-            console.log('error in creating task');
-        }
+// create task with async await and mailer call
+module.exports.create = async function(req,res){
+    try {
+        let task = await Task.create({
+            description: req.body.description,
+            category: req.body.category,
+            date: req.body.date,
+            status: 'incomplete',
+            user: req.user._id
+        });
 
+        task = await task.populate('user', 'fname lname email').execPopulate();
+        taskMailer.newTask(task);
+        
         req.flash('success','Task Created!!');
         return res.redirect('back');
-    });
+    }catch(err){
+        console.log('error in creating task');
+        req.flash('error', err);
+        return;   
+    }
 }
+
+// create task
+// module.exports.create = function(req,res){
+//     Task.create({
+//         description: req.body.description,
+//         category: req.body.category,
+//         date: req.body.date,
+//         status: 'incomplete',
+//         user: req.user._id
+//     },function(err){
+//         if(err){
+//             console.log('error in creating task');
+//         }
+
+//         req.flash('success','Task Created!!');
+//         return res.redirect('back');
+//     });
+// }
 
 // marking an existing task as completed
 module.exports.complete = function(req,res){
